1119aad32e01520ddf72425a8cb1daccbe91b442
Agilesh-s <agileshsivakumar.s@gmail.com>
2022-10-13 10:43:16 +0530

    View Airlines

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,4 +11,8 @@ export class ApiService {
   airlineData = ()=>{
     return this.http.get("https://api.instantwebtools.net/v1/passenger?page=0&size=30")
   }
+
+  airlineDatas = ()=>{
+    return this.http.get("https://api.instantwebtools.net/v1/airlines")
+  }
 }
