32e7376c8da01b933bc4df3d804a1bf1f4f3d234
Agilesh-s <agileshsivakumar.s@gmail.com>
2022-10-13 10:30:11 +0530

    View Passengers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ViewPassengersComponent } from './view-passengers/view-passengers.component';
+import { HttpClientModule } from '@angular/common/http';
+
+const appRoutes:Routes = [
+  {
+    path:"",component:ViewPassengersComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ViewPassengersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
